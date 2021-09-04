4b30195c83cc4b0ff5e558e66a2ba84b50675267
Lrd134 <larryc3200@gmail.com>
2021-09-04 03:03:44 -0400

    commit tree e2649e244278947663e278bb2d9978b1320a7a63

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const text = document.getElementById('text');
+  text.textContent = "This is really cool!";
+});
+console.log("YEs. I'm before the DOMContentLoaded event.");
